7884711455dbfe4464c01c3a51945d5204b7a3c1
pornchaithurach <pornchaithurach@gmail.com>
2026-06-28 16:52:26 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 const nextConfig = {
   output: "export",
   trailingSlash: true,
-
+  // ปิดการใช้งาน Image Optimization ชั่วคราว เนื่องจากต้องใช้เซิร์ฟเวอร์ในการทำงาน
+  images: {
+    unoptimized: true, 
+  },
   // 🔥 สำคัญมากสำหรับ GitHub Pages
   basePath: "/AgriLink/out",
   assetPrefix: "/AgriLink/out/",
